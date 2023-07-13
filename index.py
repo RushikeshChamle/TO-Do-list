9e9f087cec802bf1ab59fb2cfe4c116ba9bdc42c
RushikeshChamle <rushikeshchamle23@gmail.com>
2023-07-13 21:26:28 +0530

    NEw Pythoncode

--- index.py
+++ index.py
@@ -28,6 +28,62 @@ def insert():
 insert()
 
 
+def show_menu():
+    print("Menu:")
+    print("1. Add Task")
+    print("2. View Tasks")
+    print("3. Mark Task as Completed")
+    print("4. Exit")
+
+
+def add_task(tasks):
+    task = input("Enter the task: ")
+    tasks.append(task)
+    print("Task added successfully!")
+
+
+def view_tasks(tasks):
+    if not tasks:
+        print("No tasks found.")
+    else:
+        print("Tasks:")
+        for index, task in enumerate(tasks, start=1):
+            print(f"{index}. {task}")
+
+
+def mark_completed(tasks):
+    view_tasks(tasks)
+    task_index = int(input("Enter the task number to mark as completed: "))
+    if 1 <= task_index <= len(tasks):
+        del tasks[task_index - 1]
+        print("Task marked as completed!")
+    else:
+        print("Invalid task number.")
+
+
+def main():
+    tasks = []
+    while True:
+        show_menu()
+        choice = input("Enter your choice (1-4): ")
+        if choice == "1":
+            add_task(tasks)
+        elif choice == "2":
+            view_tasks(tasks)
+        elif choice == "3":
+            mark_completed(tasks)
+        elif choice == "4":
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
 
 
 
